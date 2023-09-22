a5020d4647af4d8271bb153aadb87f746b23e108
fahad <fahadzardari111@gmail.com>
2023-09-23 01:12:22 +0500

    database connection config added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import compression from 'compression';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import compression from "compression";
+import connectToDatabase from "./db/db.js";
 
 // initialization and configuration
 const app = express();
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(compression());
-dotenv.config();
+connectToDatabase();
 
 // routes
-
-
-
-
-
 
 // fallback route
 app.use((req, res) => {
   res.status(404).json({ message: `Route ${req.url} Not found.` });
 });
-
 
 export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 import app from "./app.js";
-
+import connectToDatabase from "./db/db.js";
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
-});
+const startServer = async () => {
+  // const isDatabaseConnected = await connectToDatabase(); for production
+  const isDatabaseConnected = true; // for development
+
+  if (isDatabaseConnected) {
+    app.listen(port, () => {
+      console.log(`Server running on port http://localhost:${port}`);
+    });
+  }
+};
+
+startServer();
